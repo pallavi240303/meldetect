f5b5e40768e360615e98a3c7de2bac148f62008d
Pallavi <24.pallavi.2003@gmail.com>
2025-05-17 06:15:21 +0530

    endpoint updated

--- src/MelanomaDetection.js
+++ src/MelanomaDetection.js
@@ -115,7 +115,7 @@ const MelanomaDetection = () => {
       }
 
           try {
-              const response = await fetch(`http://127.0.0.1:8000/predict`, {
+              const response = await fetch(`https://meldetect.onrender.com/predict`, {
                   method: 'POST',
                   headers: {
                       'Content-Type': 'application/octet-stream',
